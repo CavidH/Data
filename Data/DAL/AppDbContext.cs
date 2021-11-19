5765280c0196615949f82ab9179fd1c596ab6b27
cavid <hcavid386@gmail.con>
2021-11-19 19:15:13 +0400

    Add project files.

---
+++ Data/DAL/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Data.Models;
+
+namespace Data.DAL
+{
+    internal class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { 
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-9PAGU9P;Database=School;Trusted_Connection=True;");
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
